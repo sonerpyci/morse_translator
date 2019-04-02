c95d9e9fcd6c79738aad41e7a59b9176d7b0f42f
Soner Paycı <sonerpayci@gmail.com>
2019-04-02 21:09:00 +0300

    Server initialized with a /status endpoint to check if it works. Also added some packages to getting more valuable informations

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+/* const http = require('http').Server(app)
+const io = require('socket.io')(http) */
 
+app.use(bodyParser.json())
+app.use(morgan('combined')) // istek geldiğinde, gönderen tarafın bilgilerini console'a yazar.
 
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'It Seems Working Fine!'
+  })
+})
+
+app.listen(process.env.PORT || 3000)
